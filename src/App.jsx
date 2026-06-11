c8111a759381abbaeba029583b9ca8515a2a62d3
Ron-sagiv <pedrozeeg@gmail.com>
2026-06-11 14:26:15 +0200

    Created dev branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 const App = () => {
   return (
     <div>
-      {' '}
       <h1 className="text-3xl font-bold underline">React + TailwindCSS</h1>
-      <p>
-        <button class="btn">Button</button>
-      </p>
     </div>
   );
 };
