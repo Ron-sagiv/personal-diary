35ca989c9730166cffdbdd6658799860ed6aeb2c
Ron-sagiv <pedrozeeg@gmail.com>
2026-06-11 14:09:12 +0200

    Installed Daisy ui as plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">React + TailwindCSS</h1>;
+  return (
+    <div>
+      {' '}
+      <h1 className="text-3xl font-bold underline">React + TailwindCSS</h1>
+      <p>
+        <button class="btn">Button</button>
+      </p>
+    </div>
+  );
 };
 
 export default App;
