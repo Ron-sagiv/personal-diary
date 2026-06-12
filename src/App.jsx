4a6fa2f78e7c85d7972521c59c03fe0024f7302d
Ron-sagiv <pedrozeeg@gmail.com>
2026-06-12 16:27:33 +0200

    Try to organize some mess

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import EntryForm from './components/EntryForm';
+import AddEntryModal from './components/AddEntryModal';
 const App = () => {
   return (
     <div>
-      {' '}
-      <h1 className="text-3xl font-bold underline">React + TailwindCSS</h1>
-      <p>
-        <button class="btn">Button</button>
-      </p>
+      <h1 className="text-3xl font-bold ">My Diary</h1>
+      <AddEntryModal />
+      {/* <EntryForm /> */}
     </div>
   );
 };
